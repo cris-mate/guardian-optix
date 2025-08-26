e4e72e580a25f6b54cd2629af26bc5d2ca3c4744
cristian <cristidragosmatei@gmail.com>
2025-08-26 21:34:33 +0100

    bug corrected

--- guardian-optix-backend/middleware/authMiddleware.js
+++ guardian-optix-backend/middleware/authMiddleware.js
@@ -1,16 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ message: 'Access denied' });
+    const authHeader = req.header('Authorization');
+    if (!authHeader) return res.status(401).json({ message: 'Access denied, no token provided' });
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.status(400).json({ message: 'Invalid token' });
-  }
+    const token = authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'Access denied, token malformed' });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid token' });
+    }
 };
 
 module.exports = authMiddleware;
