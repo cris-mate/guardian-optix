47eae4fc8f5763281fd1afa940218a1dac768845
cristian <cristidragosmatei@gmail.com>
2025-08-26 20:49:45 +0100

    protect task routes - routes/taskRoutes.js

--- guardian-optix-backend/routes/taskRoutes.js
+++ guardian-optix-backend/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Task = require('../models/Task');
+const authMiddleware = require('../middleware/authMiddleware');
 
-// Create a new task
-router.post('/tasks', async (req, res) => {
+// Create a new protected task
+router.post('/tasks', authMiddleware, async (req, res) => {
   try {
     const { taskName, assignedTo, dueDate, priority } = req.body;
     const newTask = new Task({ taskName, assignedTo, dueDate, priority });
@@ -15,7 +16,7 @@ router.post('/tasks', async (req, res) => {
 });
 
 // Get all tasks
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', authMiddleware, async (req, res) => {
   try {
     const tasks = await Task.find().populate('assignedTo', 'username');
     res.status(200).json(tasks);
