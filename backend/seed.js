1b6144b29e79be3e1d4115b34f84f33040592232
cristian <cristidragosmatei@gmail.com>
2025-09-19 23:06:06 +0100

    feat(user): Enhance user model with required fields and unique username

--- backend/seed.js
+++ backend/seed.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const User = require('./models/user');
+const User = require('./models/User');
 require('dotenv').config(); // Load environment variables
 
 // --- CONFIGURATION ---
@@ -11,38 +11,41 @@ const ADMIN_PASSWORD = 'secret';
 const dbURI = process.env.DB_URI || 'mongodb://localhost:27017/guardian-optix-db';
 
 const seedAdmin = async () => {
-    try {
-        await mongoose.connect(dbURI);
-        console.log('MongoDB connected for seeding...');
+  try {
+    await mongoose.connect(dbURI);
+    console.log('MongoDB connected for seeding...');
 
-        // Check if an admin user already exists
-        const existingAdmin = await User.findOne({ email: ADMIN_EMAIL });
-        if (existingAdmin) {
-            console.log('Admin user already exists. No action taken.');
-            return;
-        }
-
-        // If no admin exists, create one
-        console.log('Admin user not found, creating one...');
-        const hashedPassword = await bcrypt.hash(ADMIN_PASSWORD, 10);
-
-        const adminUser = new User({
-            username: ADMIN_USERNAME,
-            email: ADMIN_EMAIL,
-            password: hashedPassword,
-            role: 'admin',
-        });
-
-        await adminUser.save();
-        console.log('Admin user created successfully.');
-
-
-    } catch (error) {
-        console.log('Error during admin seeding: ', error);
-    } finally {
-        await mongoose.connection.close();
-        console.log('MongoDB connection closed.');
+    // Check if an admin user already exists
+    const existingAdmin = await User.findOne({ email: ADMIN_EMAIL });
+    if (existingAdmin) {
+      console.log('Admin user already exists. No action taken.');
+      return;
     }
+
+    // If no admin exists, create one
+    console.log('Admin user not found, creating one...');
+    const hashedPassword = await bcrypt.hash(ADMIN_PASSWORD, 10);
+
+    const adminUser = new User({
+      fullName: 'Guardian Optix Admin',
+      username: ADMIN_USERNAME,
+      email: ADMIN_EMAIL,
+      phoneNumber: '12345678',
+      postCode: 'NN1 1AA',
+      password: hashedPassword,
+      role: 'Admin',
+    });
+
+    await adminUser.save();
+    console.log('Admin user created successfully.');
+
+
+  } catch (error) {
+    console.log('Error during admin seeding: ', error);
+  } finally {
+    await mongoose.connection.close();
+    console.log('MongoDB connection closed.');
+  }
 };
 
 void seedAdmin();
